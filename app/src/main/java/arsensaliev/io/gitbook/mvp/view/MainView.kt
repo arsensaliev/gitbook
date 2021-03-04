ee6733eeb530c55cd803731ae3d5d7ecc86b2371
Arsen Saliev <arsensaliev03@gmail.com>
2021-03-04 18:51:05 +0600

    feat: add mvp arch

--- app/src/main/java/arsensaliev/io/gitbook/mvp/view/MainView.kt
+++ app/src/main/java/arsensaliev/io/gitbook/mvp/view/MainView.kt
@@ -1,4 +1,5 @@
 package arsensaliev.io.gitbook.mvp.view
 
 interface MainView {
+    fun setButtonText(index: Int, text: String)
 }
--- app/src/main/java/arsensaliev/io/gitbook/ui/MainActivity.kt
+++ app/src/main/java/arsensaliev/io/gitbook/ui/MainActivity.kt
@@ -1,11 +1,36 @@
-package arsensaliev.io.gitbook
+package arsensaliev.io.gitbook.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import arsensaliev.io.gitbook.databinding.ActivityMainBinding
+import arsensaliev.io.gitbook.mvp.presenter.MainPresenter
+import arsensaliev.io.gitbook.mvp.view.MainView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+
+    private val ui: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val presenter = MainPresenter(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(ui.root)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
+        }
+
+        ui.btnCounter1.setOnClickListener(listener)
+        ui.btnCounter2.setOnClickListener(listener)
+        ui.btnCounter3.setOnClickListener(listener)
+    }
+
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setButtonText(index: Int, text: String) {
+        when(index){
+            0 -> ui.btnCounter1.text = text
+            1 -> ui.btnCounter2.text = text
+            2 -> ui.btnCounter3.text = text
+        }
     }
 }
