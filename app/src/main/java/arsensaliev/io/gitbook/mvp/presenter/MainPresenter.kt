024c763a016a5c2e93bca15af53dd7e0e6738dda
Arsen Saliev <arsensaliev03@gmail.com>
2021-03-12 21:13:22 +0600

    fix: setText method in presenter

--- app/src/main/java/arsensaliev/io/gitbook/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/arsensaliev/io/gitbook/mvp/presenter/MainPresenter.kt
@@ -1,6 +1,5 @@
 package arsensaliev.io.gitbook.mvp.presenter
 
-import arsensaliev.io.gitbook.R
 import arsensaliev.io.gitbook.mvp.model.CountersModel
 import arsensaliev.io.gitbook.mvp.view.MainView
 
@@ -14,11 +13,11 @@ class MainPresenter(val view: MainView) {
 
     fun counterTwoClick() {
         val nextValue = model.next(1)
-        view.setButtonOneText(nextValue.toString())
+        view.setButtonTwoText(nextValue.toString())
     }
 
     fun counterThreeClick() {
         val nextValue = model.next(2)
-        view.setButtonOneText(nextValue.toString())
+        view.setButtonThreeText(nextValue.toString())
     }
 }
