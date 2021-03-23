17503498114b609d42d47e7dcabf295c0ade6f95
Arsen Saliev <arsensaliev03@gmail.com>
2021-03-23 11:05:31 +0600

    feat: add fields шin GithubUser

--- app/src/main/java/arsensaliev/io/gitbook/mvp/model/entity/GithubUser.kt
+++ app/src/main/java/arsensaliev/io/gitbook/mvp/model/entity/GithubUser.kt
@@ -1,7 +1,12 @@
 package arsensaliev.io.gitbook.mvp.model.entity
 
 import android.os.Parcelable
+import com.google.gson.annotations.Expose
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class GithubUser(val login: String) : Parcelable
+data class GithubUser(
+    @Expose val id: String? = null,
+    @Expose val login: String? = null,
+    @Expose val avatarUrl: String? = null
+) : Parcelable
